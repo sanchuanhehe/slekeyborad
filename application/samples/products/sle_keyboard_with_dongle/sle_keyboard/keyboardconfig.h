7c2dcc79191af245fd02801fcb924f4563d25ca1
sanchuanhehe <2947512113@qq.com>
2024-11-12 04:49:09 +0000

    fix: 修正键盘配置中的按键映射顺序

--- application/samples/products/sle_keyboard_with_dongle/sle_keyboard/keyboardconfig.h
+++ application/samples/products/sle_keyboard_with_dongle/sle_keyboard/keyboardconfig.h
@@ -122,8 +122,8 @@
             {HID_KEY_F5,  HID_KEY_EQUALS, HID_KEY_GRAVE_ACCENT, HID_KEY_P, HID_KEY_G,                                  \
              HID_KEY_END, HID_KEY_B,      HID_KEY_NONE},                                                               \
             {HID_KEY_F4, HID_KEY_MINUS, HID_KEY_TAB, HID_KEY_O, HID_KEY_F, HID_KEY_UP_ARROW, HID_KEY_V, HID_KEY_NONE}, \
-            {HID_KEY_F3, HID_KEY_0, HID_KEY_Q, HID_KEY_I, HID_KEY_D, HID_KEY_SHIFT_RIGHT, HID_KEY_C, HID_KEY_NONE},    \
-            {HID_KEY_F2, HID_KEY_9, HID_KEY_W, HID_KEY_U, HID_KEY_S, HID_KEY_SLASH, HID_KEY_X, HID_KEY_NONE},          \
+            {HID_KEY_F3, HID_KEY_0, HID_KEY_W, HID_KEY_I, HID_KEY_D, HID_KEY_SHIFT_RIGHT, HID_KEY_C, HID_KEY_NONE},    \
+            {HID_KEY_F2, HID_KEY_9, HID_KEY_Q, HID_KEY_U, HID_KEY_S, HID_KEY_SLASH, HID_KEY_X, HID_KEY_NONE},          \
             {HID_KEY_F1, HID_KEY_8, HID_KEY_E, HID_KEY_Y, HID_KEY_A, HID_KEY_PERIOD, HID_KEY_Z, HID_KEY_NONE},         \
             {HID_KEY_ESC,       HID_KEY_7,     HID_KEY_R,          HID_KEY_T,                                          \
              HID_KEY_CAPS_LOCK, HID_KEY_COMMA, HID_KEY_SHIFT_LEFT, HID_KEY_NONE},                                      \
